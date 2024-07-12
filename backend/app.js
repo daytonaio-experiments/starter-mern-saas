2ce1c7e911e947a3fc3a6d99220ebd49f0da0932
Anurag Kainth <anuragkainth07@gmail.com>
2024-07-12 18:53:18 +0000

    fix: payload size fix

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const customerRoutes = require('./routes/customerRoutes');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.json());
 app.use(cors({origin: '*'}));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.get('/test', (req, res) => {
     res.send('Hello from server!');
